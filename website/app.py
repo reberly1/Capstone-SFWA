ad5dec4cb584a32925fffebddc4f4cecf1de9193
reberly1 <reberly1@umbc.edu>
2024-03-26 15:57:08 -0400

    Form Configuration

--- website/app.py
+++ website/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-        return render_template('home.html', title="Home")
+    return render_template('home.html', title="Home")
 
 @app.route('/calculator', methods=['GET','POST'])
 def calculator():
@@ -24,28 +24,23 @@ def unguided():
     return render_template('unguided.html')
 
 @app.route('/calculator/debt', methods=['GET','POST'])
-def about():
-    if request.method == 'POST':
-        principal = request.form['principal[]']
-        interest = request.form['interest[]']
-        grad = request.form['grad']
-        term = request.form['term']
-        future = request.form['future']
-        return render_template('debt.html', principal=principal, interest=interest, grad=grad, term=term,future=future)
-    else:
-        return render_template("debt.html")
+def debt():
+    return render_template("debt.html")
     
 @app.route('/calculator/report', methods=['GET','POST'])
 def report():
     if request.method == 'POST':
-        principal = request.form['principal[]']
-        interest = request.form['interest[]']
+        principal = request.form.getlist('principal[]')
+        interest = request.form.getlist('interest[]')
+        subsidized = request.form.getlist('subsidized[]')
         grad = request.form['grad']
         term = request.form['term']
         future = request.form['future']
+
         # Process the data (you can do whatever you want with it)
         print('Principal:', principal)
         print('Interest:', interest)
+        print('subsidized', subsidized)
         print('Graduation Date:', grad)
         print('Term Cost:', term)
         print('Future Interest:', future)
